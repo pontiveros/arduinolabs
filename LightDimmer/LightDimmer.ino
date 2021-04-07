c966f6217c78c3f3654beae56ea6ac4309bd1e85
Pedro Ontiveros <ontiveros.pedro@gmail.com>
2021-04-07 00:45:55 -0400

    Add LightDimmer sketch

---
+++ LightDimmer/LightDimmer.ino
@@ -0,0 +1,23 @@
+
+/**********************************/
+/* Pedro Ontiveros                */
+/* pontiveros.developer@gmail.com */
+/**********************************/
+
+/* Variables */
+
+int potPin = A0;  // Analog imput pint connected to potentiometer
+int potValue = 0; // Value to be read from potentiometer
+int led = 9;      // Led output
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(let, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  potValue = analogRead(potPin);
+  analogWrite(led, potValue/4);
+  delay(10);
+}
