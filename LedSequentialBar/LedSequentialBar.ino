f903b7766a48cff2657e4b74f3200dec8da13025
Pedro Ontiveros <ontiveros.pedro@gmail.com>
2021-04-09 22:24:04 -0400

    Update LedSequentialBar program

--- LedSequentialBar/LedSequentialBar.ino
+++ LedSequentialBar/LedSequentialBar.ino
@@ -9,7 +9,7 @@
 const int analogPin = A0;
 const int   ledSize = 9;
 const int ledPins[] = {2,3,4,5,6,7,8,9,10};
-int flag = 0;
+int f1 = 1;
 
 void setup() {
   for (int index = 0; index < ledSize; index++) {
@@ -21,12 +21,29 @@ void loop() {
   int   sensor = analogRead(analogPin);
   int ledLevel = map(sensor, 0, 1023, 0, ledSize);
 
-  for (int index = 0; index < ledSize; index++) {
-    digitalWrite(ledPins[index], (flag == 0) ? LOW : HIGH);
-    delay(ledLevel*10);
+  if (f1 == 1) {
+    goForward(1, (ledLevel*10));
+    goForward(0, (ledLevel*10));
+  } else {
+    goBackward(1, (ledLevel*10));
+    goBackward(0, (ledLevel*10));
   }
 
-  flag = !flag;
+  f1 = (f1 == 1) ? 0 : 1;
   
   delay(ledLevel);
+}
+
+void goForward(int value, int pause) {
+  for (int index = 0; index < ledSize; index++) {
+      digitalWrite(ledPins[index], (value == 1) ? HIGH : LOW);
+      delay(pause);
+    }
+}
+
+void goBackward(int value, int pause) {
+  for (int index = (ledSize - 1); index >= 0; index--) {
+      digitalWrite(ledPins[index], (value == 1) ? HIGH : LOW);
+      delay(pause);
+    }  
 }
